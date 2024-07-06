aa061e826e567a32e103b1fc54eedaa56e6e820d
Juan Alma <juanangelaalma@gmail.com>
2024-07-06 22:09:34 +0700

    Created models and migrations

---
+++ database/migrations/2024_07_06_150130_create_education_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->smallInteger('attainment');
+            $table->string('school', 100);
+            $table->string('from', 50);
+            $table->string('to', 50);
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
